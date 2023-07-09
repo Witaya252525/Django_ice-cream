1c07f8e9f24f009b6e35fa7f40aeff99d5c1cf47
Witaya252525 <witaya.c@kkumail.com>
2023-07-09 23:31:58 +0700

    add foods page

--- ecommerce2/app_foods/views.py
+++ ecommerce2/app_foods/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.http.response import HttpResponse
 
 # Create your views here.
+
+def foods(request):
+    return HttpResponse ("อาหาร อร่อย   ส่งรวดเร็ว")
+
--- ecommerce2/app_general/views.py
+++ ecommerce2/app_general/views.py
@@ -2,5 +2,10 @@ from django.shortcuts import render
 from django.http.response import HttpResponse
 
 # Create your views here.
+
 def home(request):
     return HttpResponse ("จันทร์ไรเดอร์")
+
+
+def about(request):
+    return HttpResponse ("เกี่ยวกับเรา")
