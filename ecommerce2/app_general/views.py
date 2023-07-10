9fc97182bce4234b4c78a88b501c4ec1a9c1ce3c
Witaya252525 <witaya.c@kkumail.com>
2023-07-10 07:24:54 +0700

    Add template

--- ecommerce2/app_general/views.py
+++ ecommerce2/app_general/views.py
@@ -3,8 +3,12 @@ from django.http.response import HttpResponse
 
 # Create your views here.
 
+# def home(request):
+#     return HttpResponse ("จันทร์ไรเดอร์")
+
+
 def home(request):
-    return HttpResponse ("จันทร์ไรเดอร์")
+    return render(request,'app_general/home.html')
 
 
 def about(request):
