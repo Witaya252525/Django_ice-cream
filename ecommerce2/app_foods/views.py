228cd0acd2b268ce6443456a3b4352e87490a10a
Witaya252525 <witaya.c@kkumail.com>
2024-04-12 05:08:48 +0700

    filter

--- ecommerce2/app_foods/views.py
+++ ecommerce2/app_foods/views.py
@@ -11,13 +11,33 @@ from django.http.response import HttpResponse
 #     return HttpResponse ("เมนู ID = "  + str(food_id))
 
 
+all_foods = [
+
+        {'id':1 ,'title':'Dark Chore  Premium' ,'price':499 ,'is_premium':True},
+        {'id':2 ,'title':'Red spicy' ,'price':349 ,'is_premium':False},
+        {'id':3 ,'title':'Blue Glacier' ,'price':349 ,'is_premium':False},
+
+]
+
+
+
+
+
 def foods(request):
-    return render(request,'app_foods/foods.html')
+    context = { 'foods': all_foods}
+    return render(request,'app_foods/foods.html' ,context)
 
 def about(request):
     return render(request,'app_foods/about.html')
 
 
 def food(request ,food_id):
-    return render(request,'app_foods/food.html' , context = {"food_id" : food_id })
+    one_food = None
+    try:
+        one_food = [f for f in all_foods if f['id'] == food_id][0]
+    except IndexError :
+        print('ไม่มีเทอ ไม่ม่เทอ')
+    
+    context = {'food': one_food }
+    return render(request,'app_foods/food.html' , context )
 
